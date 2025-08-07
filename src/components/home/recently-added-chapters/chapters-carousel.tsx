e1770dd4a9a10cf0605a38e492e5a189bd6f5b3e
Miralhas <victor2.miralhas@gmail.com>
2025-08-06 22:27:31 -0300

    style: change border color on hover

--- src/components/home/recently-added-chapters/chapters-carousel.tsx
+++ src/components/home/recently-added-chapters/chapters-carousel.tsx
@@ -27,14 +27,14 @@ const ChaptersCarousel = ({ chapters }: { chapters: RecentlyAddedChapter[] }) =>
           <CarouselItem key={index} className="basis-full">
             <div className="w-full flex flex-col gap-1">
               {chunk.map(chapter => (
-                <div key={chapter.id} className="w-full border-b border-primary/40 pb-2 pt-1 flex gap-2 transition-all duration-300 group hover:-translate-y-1">
+                <div key={chapter.id} className="w-full border-b border-primary/40 hover:border-primary/80 pb-2 pt-1 flex gap-2 transition-all duration-300 group hover:-translate-y-1">
                   <ChapterImage {...chapter} />
                   <div className="w-full md:grid md:grid-cols-[0.5fr_0.5fr_0.3fr] md:gap-3 capitalize tracking-tight font-semibold duration-300 transition-colors group-hover:text-accent">
                     <div className="col-span-1 md:flex md:flex-col">
                       <Link href="/" className="text-sm text-[15.2px] text-ellipsis whitespace-break-spaces line-clamp-1 md:line-clamp-2 w-fit text-white group-hover:text-inherit">
                         {chapter.novelTitle}
                       </Link>
-                      <div className="text-sm text-[13px] font-normal text-muted-foreground mt-1 hidden md:block">
+                      <div className="text-sm text-[13px] text-muted-foreground mt-1 hidden md:block">
                         <p className="line-clamp-2">{chapter.author}</p>
                       </div>
                     </div>

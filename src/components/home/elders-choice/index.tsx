26c84ad4ce347c68d3f4aa2f0e974e82a16c0dd0
Miralhas <victor2.miralhas@gmail.com>
2025-08-07 11:08:36 -0300

    feat: elders choice carousel

--- src/components/home/elders-choice/index.tsx
+++ src/components/home/elders-choice/index.tsx
@@ -1,23 +1,14 @@
-import NovelCard from "@/components/novel-card";
 import SectionHeader from "@/components/section-header";
 import { getEldersChoice } from "@/services/novels/server-queries";
 import { GraduationCap } from "lucide-react";
-import Link from "next/link";
-
-const SIZE = 6;
+import ChoicesCarousel from "./choices-carousel";
 
 const EldersChoice = async () => {
   const res = await getEldersChoice();
   return (
     <section className="w-full space-y-6">
       <SectionHeader icon={GraduationCap} title="Elder's Choice" />
-      <div className="grid grid-cols-3 md:grid-cols-6 gap-y-6 gap-x-4 grid-rows-1" id="elders-choice-grid">
-        {res.slice(0, SIZE).map(choice => (
-          <Link className="group space-y-2 [&:nth-child(n+7)]:hidden md:[&:nth-child(n+7)]:block" key={choice.id} href="/">
-            <NovelCard novelSummary={choice.novel} size="lg" />
-          </Link>
-        ))}
-      </div>
+      <ChoicesCarousel choices={res} />
     </section>
   )
 }

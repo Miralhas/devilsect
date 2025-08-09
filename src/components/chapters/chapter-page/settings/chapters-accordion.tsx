93368b213a76d3c2c897890e461e1ebba01c90dd
Miralhas <victor2.miralhas@gmail.com>
2025-08-09 01:17:24 -0300

    feat: chapter sheet accordions

--- src/components/chapters/chapter-page/settings/chapters-accordion.tsx
+++ src/components/chapters/chapter-page/settings/chapters-accordion.tsx
@@ -3,25 +3,34 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import { arrayChunker, cn } from "@/lib/utils";
+} from "@/components/ui/accordion";
+import { arrayChunker } from "@/lib/utils";
+import { ChapterSummary } from "@/types/chapter";
+import { PaginatedQuery } from "@/types/pagination";
+import { format } from "date-fns";
+import Link from "next/link";
 
-const ChapterAccordion = () => {
+const ChapterAccordion = ({ paginatedSumaries }: { paginatedSumaries: PaginatedQuery<ChapterSummary[]> }) => {
   return (
     <div className="w-full space-y-2">
       <Accordion
         type="single"
         collapsible
-        className="w-full"
-        defaultValue="item-1"
+        className="w-full pt-1"
       >
-        {arrayChunker(Array.from({ length: 250 }), 100).map((_, index) => (
-          <AccordionItem value={"item-1" + index} key={index}>
-            <AccordionTrigger>{_.length}</AccordionTrigger>
-            <AccordionContent className="flex flex-col gap-4 text-balance">
-              {_.map((x, i) => (
-                <div key={i} className={cn("h-8 border")}>
-                </div>
+        {arrayChunker(paginatedSumaries!.results, 100).map((chapterSummaryArr, index) => (
+          <AccordionItem value={String(index)} key={index} className="tracking-tight">
+            <AccordionTrigger className="[&[data-state=open]]:bg-secondary p-4 text-[15px] text-white font-semibold">
+              {chapterSummaryArr[0].number} - {chapterSummaryArr[chapterSummaryArr.length - 1].number}
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance first:mt-3">
+              {chapterSummaryArr.map(chapter => (
+                <Link href="/" key={chapter.id} className="border-b-2 px-4 pb-4">
+                  <span className="text-white text-base font-semibold line-clamp-1">
+                    {chapter.title}
+                  </span>
+                  <span className="text-[12px] text-muted-foreground">{format(new Date(chapter.createdAt), 'MM/dd/yyyy')}</span>
+                </Link>
               ))}
             </AccordionContent>
           </AccordionItem>
--- src/components/chapters/chapter-page/settings/show-chapters-sheet.tsx
+++ src/components/chapters/chapter-page/settings/show-chapters-sheet.tsx
@@ -10,24 +10,27 @@ import {
 import { env } from "@/env";
 import defaultBlur from "@/lib/blur-data";
 import { Chapter } from "@/types/chapter";
-import { BookOpenText, Menu } from "lucide-react";
+import { BookOpenText, Loader, Menu } from "lucide-react";
 import Image from "next/image";
 import ChapterAccordion from "./chapters-accordion";
+import { useGetNovelChapterSummaries } from "@/services/chapters/client-queries";
 
 const ShowChaptersSheet = ({ chapter }: { chapter: Chapter }) => {
+  const { data, isLoading, isError } = useGetNovelChapterSummaries({ novelSlug: chapter.novelSlug, page: 0, size: 9999 });
+  
   return (
     <>
       <Sheet>
         <SheetTrigger asChild>
           <div className="flex gap-3 items-center cursor-pointer">
-            <Menu className="text-muted-foreground size-6" />
+            <Menu className="text-muted-foreground size-7" />
             <p className="text-sm md:text-[15px] font-semibold tracking-tight leading-7 text-white line-clamp-1 w-full max-w-[100px] xs:max-w-[190px] md:max-w-[300px]">
               {chapter.title}
             </p>
           </div>
         </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader className="grid grid-cols-4 mt-5 gap-3 pb-4 border-b">
+        <SheetContent side="left" className="gap-0">
+          <SheetHeader className="grid grid-cols-4 mt-5 gap-3 pb-4 border-b mb-0">
             <div className="col-span-1 w-full relative aspect-[3/4]">
               <Image
                 src={`${env.NEXT_PUBLIC_BASE_URL}/novels/${chapter.novelSlug}/image`}
@@ -39,18 +42,17 @@ const ShowChaptersSheet = ({ chapter }: { chapter: Chapter }) => {
             </div>
             <div className="col-span-3 flex gap-1 flex-col">
               <SheetTitle className="capitalize text-sm md:text-base tracking-tight leading-6 font-semibold w-full line-clamp-3">
-                {/* {chapter.novelSlug.replaceAll("-", " ")} */}
-                My Journey to Immortality Begins with Hunting
+                {chapter.novelSlug.replaceAll("-", " ")}
               </SheetTitle>
-              <div className="hidden xs:flex items-center gap-2 text-muted-foreground">
+              {!isLoading && !isError && <div className="hidden xs:flex items-center gap-1.5 text-muted-foreground">
                 <BookOpenText className="size-4" />
-                <p className="text-sm mb-1">300</p>
-              </div>
+                <p className="text-[12px] mb-0.5">{data?.totalItems} Chapters</p>
+              </div>}
             </div>
           </SheetHeader>
-          <div className="grid flex-1 auto-rows-min gap-6 px-4 overflow-y-auto">
-            <ChapterAccordion />
-          </div>
+          {!isLoading && !isError ? <div className="grid flex-1 auto-rows-min gap-6 px-4 overflow-y-auto">
+            <ChapterAccordion paginatedSumaries={data!} />
+          </div> : <div className="w-full grid place-items-center min-h-[10vh]"><Loader className="size-7 animate-spin" /></div>}
           <SheetFooter>
             <SheetClose asChild>
             </SheetClose>

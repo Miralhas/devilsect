2e252ff75816f8af8e351037c6b784859ac48f13
Miralhas <victor2.miralhas@gmail.com>
2025-08-13 23:29:35 -0300

    chore: remove unused ref

--- src/components/novel/about-section/tabs.tsx
+++ src/components/novel/about-section/tabs.tsx
@@ -1,19 +1,16 @@
 'use client'
 
 import { Novel } from "@/types/novel";
-import { BookOpenText, UserIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
-import { useRef, useState } from "react";
-import NovelTags from "./novel-tags";
-import NovelSummary from "./novel-summary";
-import NovelDescription from "./novel-description";
+import { useState } from "react";
+import AboutTab from "./about-tab";
+import ChaptersTab from "./chapters-tab";
 
 const tabs = ["About", "Chapters"] as const;
 type Tabs = typeof tabs[number];
 
 const Tabs = ({ novel }: { novel: Novel }) => {
   const [selectedTab, setSelectedTab] = useState<Tabs>(tabs[0]);
-  const divRef = useRef<HTMLDivElement>(null);
 
   return (
     <>
@@ -49,45 +46,10 @@ const Tabs = ({ novel }: { novel: Novel }) => {
           exit={{ y: -10, opacity: 0 }}
           transition={{ duration: 0.2 }}
         >
-          {selectedTab === "About" && (
-            <div className="w-full max-w-[1024px] mx-auto border-b border-zinc-50/25 pb-6 space-y-8" ref={divRef}>
-              <div className="md:flex gap-34 ">
-                <div className="space-y-1 flex md:flex-col gap-2 md:gap-0">
-                  <div className="flex items-center gap-1">
-                    <BookOpenText className="size-4 mt-0.25 text-muted-foreground" />
-                    <p className="text-muted-foreground text-sm md:text-[15px] font-medium tracking-tight">Chapters</p>
-                  </div>
-                  <div className="flex items-center gap-1">
-                    <p className="text-sm md:text-base font-semibold line-clamp-1 mb-0.5">{novel.chaptersCount} Chapters</p>
-                  </div>
-                </div>
-
-                <div className="space-y-1 flex md:flex-col gap-2 md:gap-0">
-                  <div className="flex items-center gap-1">
-                    <UserIcon className="size-4 text-muted-foreground" />
-                    <p className="text-muted-foreground text-sm md:text-[15px] font-medium tracking-tight">Author</p>
-                  </div>
-                  <div className="flex items-center gap-1">
-                    <p className="text-sm md:text-base font-semibold mb-1 line-clamp-1">{novel.author}</p>
-                  </div>
-                </div>
-              </div>
-              <NovelDescription novel={novel} />
-              <NovelSummary description={novel.description} />
-              <NovelTags tags={novel.tags} />
-            </div>
-          )}
-
-          {selectedTab === "Chapters" && (
-            <div className="w-full max-w-[1024px] mx-auto flex gap-4 flex-col">
-              {Array.from({ length: 10 }).map((_, index) => (
-                <div className="h-10 bg-primary/40 border border-accent w-full" key={index}></div>
-              ))}
-            </div>
-          )}
+          {selectedTab === "About" && <AboutTab novel={novel} />}
+          {selectedTab === "Chapters" && <ChaptersTab novel={novel} />}
         </motion.div>
       </AnimatePresence>
-
     </>
   )
 }

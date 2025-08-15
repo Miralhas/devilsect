605b978f5d0e31ff5196eb5a24b68d1a4d25bc50
Miralhas <victor2.miralhas@gmail.com>
2025-08-15 16:10:54 -0300

    feat: add dynamic imports for client components

--- src/components/novel/about-section/tabs.tsx
+++ src/components/novel/about-section/tabs.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import SpinnerLoader from "@/components/ui/spinner-loader";
 import { Novel } from "@/types/novel";
 import { AnimatePresence, motion } from "motion/react";
+import dynamic from 'next/dynamic';
 import { useState } from "react";
-import AboutTab from "./about-tab";
-import ChaptersTab from "./chapters-tab";
+
+const AboutTab = dynamic(() => import("./about-tab"));
+const ChaptersTab = dynamic(() => import("./chapters-tab"), { ssr: false, loading: () => <SpinnerLoader containerClassName="min-h-[30vh]" /> });
+
 
 const tabs = ["About", "Chapters"] as const;
 type Tabs = typeof tabs[number];

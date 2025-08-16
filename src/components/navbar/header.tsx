976744ec9ed485a4801f87e0ca33b01926f39f93
Miralhas <victor2.miralhas@gmail.com>
2025-08-16 18:16:41 -0300

    feat: dynamic import loading

--- src/components/navbar/header.tsx
+++ src/components/navbar/header.tsx
@@ -9,10 +9,11 @@ import LogoLink from "../logo-link";
 import SpinnerLoader from "../ui/spinner-loader";
 import MenuButton from "./menu-button";
 import SearchInput from "./search-input";
+import LoginButton from "./login-button";
 
 const MobileMenu = dynamic(() => import("./mobile-menu"), { ssr: false, loading: () => <SpinnerLoader containerClassName="bg-secondary h-[400px]" /> });
 
-const UserLinks = dynamic(() => import("./user-links"), { ssr: false });
+const UserLinks = dynamic(() => import("./user-links"), { ssr: false, loading: () => <LoginButton isLoading /> });
 
 
 export type NavLink = {
--- src/components/navbar/user-links/index.tsx
+++ src/components/navbar/user-links/index.tsx
@@ -14,7 +14,7 @@ const UserLinks = () => {
   }
 
   return (
-    <div className="flex gap-3 items-center">
+    <div className="flex gap-3 items-center md:ms-3">
       <LibraryLink />
 
       <UserInbox />

6931fe8cc72d33e5a72bb3e433214bf977da9e07
Miralhas <victor2.miralhas@gmail.com>
2025-08-17 10:38:31 -0300

    feat: increase image cache TTL

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import { env } from "@/env";
 import type { NextConfig } from "next";
 import withPlaceholder from "@plaiceholder/next";
 
+const WEEK_IN_SECONDS = 604800;
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    minimumCacheTTL: WEEK_IN_SECONDS * 2,
     remotePatterns: [
       new URL('https://github.com/**'),
       new URL(`${env.APP_URL}/**`)

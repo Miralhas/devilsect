fe17f49be64569c39b1bc34a48a749db5cdd32e5
Miralhas <victor2.miralhas@gmail.com>
2025-08-21 20:11:04 -0300

    feat: loading betweem chapter pages

---
+++ src/app/(core)/novels/[slug]/[chapterSlug]/loading.tsx
@@ -0,0 +1,11 @@
+import YinYangLoading from "@/components/yinyang-loading";
+
+const ChapterLoading = () => {
+  return (
+    <>
+      <YinYangLoading />
+    </>
+  )
+}
+
+export default ChapterLoading;
---
+++ src/app/(core)/novels/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+const NovelLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default NovelLayout;

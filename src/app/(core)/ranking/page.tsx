a2e324733c652f387d1e066eb8b35ce5e1d3c1c4
Miralhas <victor2.miralhas@gmail.com>
2025-09-01 17:47:18 -0300

    fix:  wrong next.config properties

--- src/app/(core)/ranking/page.tsx
+++ src/app/(core)/ranking/page.tsx
@@ -1,4 +1,4 @@
-const RankingsPage = () => {
+const RankingPage = () => {
   return (
     <section className="min-h-screen max-w-[1280px] mx-auto">
       
@@ -6,4 +6,4 @@ const RankingsPage = () => {
   )
 }
 
-export default RankingsPage;
+export default RankingPage;

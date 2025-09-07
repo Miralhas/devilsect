78ac7ba360a11aa088773cd10ede1a33c9dff1dc
Miralhas <victor2.miralhas@gmail.com>
2025-09-07 14:30:20 -0300

    feat: google Oauth2 authentication with redirect_uri

--- src/app/api/login/google/callback/route.ts
+++ src/app/api/login/google/callback/route.ts
@@ -15,6 +15,9 @@ export async function GET(request: Request) {
   const cookieStore = await cookies();
   const storedState = cookieStore.get("google_oauth_state")?.value ?? null;
   const codeVerifier = cookieStore.get("google_code_verifier")?.value ?? null;
+  const redirectUri = cookieStore.get("redirectUri")?.value ?? "/";
+
+  cookieStore.delete("redirectUri");
 
   if (code === null || state === null || storedState === null || codeVerifier === null) {
     console.error("Google Authentication code || state || storedState || codeVerifier are null");
@@ -62,7 +65,6 @@ export async function GET(request: Request) {
   const data: ApiLoginResponse = await res.json();
 
   await createSession(data);
-  // revalidatePath("/");
 
-  redirect("/");
+  redirect(redirectUri);
 }

ef096a8a0a901e531e87e3e24484d5fd3d14f532
Miralhas <victor2.miralhas@gmail.com>
2025-09-13 11:09:38 -0300

    feat: DisabledLink component

---
+++ src/components/disabled-link.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import Link, { type LinkProps } from "next/link";
+import { PropsWithChildren } from "react";
+
+type Props<R> = LinkProps<R> & { disabled?: boolean; className?: string };
+
+const DisabledLink = <R extends string>({ children, className, disabled = false, ...rest }: PropsWithChildren<Props<R>>) => {
+  return (
+    <Link {...rest} className={cn(className, disabled && "pointer-events-none opacity-50")}>
+      {children}
+    </Link>
+  )
+}
+
+export default DisabledLink;

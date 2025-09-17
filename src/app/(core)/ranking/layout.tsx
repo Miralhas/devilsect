548763ed8ee63d4ccaa3f9293d4157f533ff4a92
Miralhas <victor2.miralhas@gmail.com>
2025-09-17 08:05:42 -0300

    feat: ranking layout

--- src/app/(core)/ranking/layout.tsx
+++ src/app/(core)/ranking/layout.tsx
@@ -1,9 +1,10 @@
+import RankingHeader from "@/components/ranking/ranking-header";
 import { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <section className="min-h-screen">
-      <div className="text-center text-2xl font-bold tracking-tight mt-12">Ranking</div>
+    <section className="grid grid-rows-[min-content_max-content] w-full max-w-[1280px] mx-auto relative pt-6 md:pt-12 min-h-screen space-y-5 p-4 pb-8">
+      <RankingHeader />
       {children}
     </section>
   )

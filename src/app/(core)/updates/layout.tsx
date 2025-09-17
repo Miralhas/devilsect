384febb0913d26ff0e8717594a6b007e0c91c0c6
Miralhas <victor2.miralhas@gmail.com>
2025-09-17 10:53:05 -0300

    feat: add page header to updates page

--- src/app/(core)/updates/layout.tsx
+++ src/app/(core)/updates/layout.tsx
@@ -1,12 +1,21 @@
+import PageHeader from "@/components/page-header";
+import { RefreshCwIcon } from "lucide-react";
 import { PropsWithChildren } from "react";
 
 const UpdatesLayout = ({ children }: PropsWithChildren) => {
   return (
     <section className="min-h-screen w-full max-w-[1280px] mx-auto p-6.5 pt-6 md:pt-10 space-y-8 md:space-y-12">
-      <div>
+      {/* <div>
         <h1 className="text-2xl font-bold tracking-tight text-balance">Recently Updated Chapters</h1>
         <h4 className="text-base text-[15px] text-zinc-400 font-medium tracking-tight text-pretty">Recently updated chapters from ongoing web novels and light novels.</h4>
-      </div>
+      </div> */}
+      <PageHeader
+        title="Recently Updated Chapters"
+        description="Recently updated chapters from ongoing web novels and light novels."
+        icon={RefreshCwIcon}
+        descriptionClassName="md:text-base"
+        titleClassName="md:text-3xl"
+      />
       {children}
     </section>
   )

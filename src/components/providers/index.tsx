f901df680900a69fcee29311e1327b5a9df2c230
Miralhas <victor2.miralhas@gmail.com>
2025-09-18 18:01:53 -0300

    feat: react quear SSR client

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,20 +1,39 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { GlobalLoginProvider } from '@/contexts/global-login-context';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 2 * 60 * 1000 // 2 minutes
-    }
+// SSR Documentation: https://tanstack.com/query/latest/docs/framework/react/guides/advanced-ssr
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000 * 2,
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
   }
-});
+}
 
 const Providers = ({ children }: PropsWithChildren) => {
-
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <AppRouterCacheProvider options={{ enableCssLayer: true }}>

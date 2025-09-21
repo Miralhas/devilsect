9655155a25619326d820c6bd8ade3ae117be8c5d
Miralhas <victor2.miralhas@gmail.com>
2025-09-21 12:21:38 -0300

    feat: add search params sate on dashboard novel section

--- src/components/dashboard/dashboard-tab/index.tsx
+++ src/components/dashboard/dashboard-tab/index.tsx
@@ -1,19 +1,23 @@
+'use client'
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import NovelInfo from "./novel-info";
 import { Separator } from "@/components/ui/separator";
 import { Novel } from "@/types/novel";
 import NovelChapters from "./novel-chapters";
+import { useQueryState } from "nuqs";
 
 const DashboardTab = ({ novel }: { novel: Novel }) => {
-  
+  const [tab, setTab] = useQueryState("tab", { defaultValue: "novel" });
+
   return (
-    <Tabs defaultValue="novel-info" className="w-full">
+    <Tabs value={tab} onValueChange={(v) => setTab(v)} className="w-full">
       <TabsList className="grid grid-cols-2 w-full border border-zinc-50/10">
-        <TabsTrigger value="novel-info">Novel Info</TabsTrigger>
+        <TabsTrigger value="novel">Novel Info</TabsTrigger>
         <TabsTrigger value="chapters">Chapters</TabsTrigger>
       </TabsList>
       <Separator className="mt-4" />
-      <TabsContent value="novel-info">
+      <TabsContent value="novel">
         <NovelInfo novel={novel} />
       </TabsContent>
       <TabsContent value="chapters">

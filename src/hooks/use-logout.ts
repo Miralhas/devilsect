0554202487e1bf8d7af0d487969c46a7a94ee346
Miralhas <victor2.miralhas@gmail.com>
2025-09-24 20:43:42 -0300

    feat: reload page after logout

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,19 +1,14 @@
 'use client'
 
 import { useLogoutMutation } from "@/services/authentication/client-mutation";
-import { useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 
 const useLogout = () => {
-  const queryClient = useQueryClient();
-  const router = useRouter();
   const mutation = useLogoutMutation();
 
   const handleLogout = async () => {
     mutation.mutate(undefined, {
       onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ['user'] });
-        router.refresh();
+        window.location.reload();
       }
     });
   }

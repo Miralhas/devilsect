def6960598b84a5a720379a025dd3d2bb30329b3
Miralhas <victor2.miralhas@gmail.com>
2025-09-26 09:02:12 -0300

    feat: tags page metadata

--- src/app/(core)/tags/page.tsx
+++ src/app/(core)/tags/page.tsx
@@ -5,14 +5,22 @@ import { TagsParams } from "@/lib/schemas/tags-schema";
 import { getTagsInitialParams, getTagsQueryOptions } from "@/services/novels/client-queries";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { TagsIcon } from "lucide-react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tags",
+  description: "Explore our collection of tags to easily find novels that match your interests.",
+};
 
 const TagsPage = async ({ searchParams }: { searchParams: Promise<{ letter?: string }> }) => {
   const { letter } = await searchParams;
+  
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery(getTagsQueryOptions({
     ...getTagsInitialParams,
     firstLetter: letter as TagsParams["firstLetter"],
   }));
+
   return (
     <div className="space-y-6">
       <PageHeader

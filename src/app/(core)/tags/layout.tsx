18f3f000e09516aafe14bad595068028211827c5
Miralhas <victor2.miralhas@gmail.com>
2025-09-25 22:29:01 -0300

    feat: tags page layout

---
+++ src/app/(core)/tags/layout.tsx
@@ -0,0 +1,15 @@
+import DynamicBreadcrumb from "@/components/dynamic-breadcrumb";
+import BlurCenter from "@/components/ui/blur-center";
+import { PropsWithChildren } from "react";
+
+const TagsLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <section className="mx-auto w-full max-w-[1280px] pt-6 min-h-screen space-y-5 p-4 pb-8">
+      <DynamicBreadcrumb />
+      <BlurCenter opacity="ultra-low" />
+      {children}
+    </section>
+  )
+}
+
+export default TagsLayout;

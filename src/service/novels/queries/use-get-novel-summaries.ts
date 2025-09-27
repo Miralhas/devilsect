7589effdeb348ab86dfa99293e5a8ce2159ec054
Miralhas <victor2.miralhas@gmail.com>
2025-09-27 14:20:38 -0300

    refactor: novel queries

---
+++ src/service/novels/queries/use-get-novel-summaries.ts
@@ -0,0 +1,27 @@
+import { NovelSummariesParams, SortKey } from "@/lib/schemas/novel-summaries-params-schema";
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { getNovelSummaries } from "../api/get-novel-summaries";
+import { novelKeys } from "./query-keys";
+
+type NovelSummaryParams = { params: NovelSummariesParams; enabled?: boolean };
+
+export const novelSummariesQueryOptions = ({ params, enabled = false }: NovelSummaryParams) => queryOptions({
+  queryFn: () => getNovelSummaries(params),
+  queryKey: novelKeys.getNovelSummaries(params),
+  enabled: () => !!params.q || enabled,
+});
+
+export const useGetNovelSummaries = (params: NovelSummaryParams) => (
+  useQuery(novelSummariesQueryOptions(params))
+);
+
+export const novelSummariesInitialParams: NovelSummariesParams = { size: 50, page: 0, sort: SortKey.BAYESIAN_RANKING };
+export const novelListInitalParams: NovelSummariesParams = {
+  genres: "",
+  size: 18,
+  q: "",
+  page: 0,
+  status: "",
+  sort: SortKey.BAYESIAN_RANKING,
+  chaptersRange: ""
+};

6980da3af6d9f170cad3f77c291a540849a2babb
Miralhas <victor2.miralhas@gmail.com>
2025-09-27 22:57:10 -0300

    feat: logo image component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,18 @@
 'use client'
 
-import Image from "next/image";
 import Link from "next/link";
-import { createWsrvLoader } from "../wsrvLoader";
+import LogoImage from "../logo-image";
 
 const Footer = () => {
   return (
     <div className="w-full bg-zinc-950 border-b-2 border-secondary shadow-2xl">
       <footer className="w-full max-w-[1280px] flex flex-col md:flex-row items-center justify-between mx-auto p-4 gap-5 md:gap-0">
         <Link href="/" className="mr-6 transition-opacity mt-[3px] items-center ease-in duration-200 flex gap-1.5 hover:opacity-80">
-          <Image fill={false} loader={createWsrvLoader({})} src="https://static.devilsect.com/devilsect-logo.png" width={25} height={25} quality={100} alt="Website logo" className="ml-2 w-auto h-7" />
+          <LogoImage
+            width={25}
+            height={25}
+            className="ml-2 w-auto h-7"
+          />
           <span className="text-base md:text-xl uppercase tracking-widest font-tilt-warp text-accent-foreground/85">
             DevilSect
           </span>
--- src/components/logo-link.tsx
+++ src/components/logo-link.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import Image from "next/image";
 import Link from "next/link";
-import { createWsrvLoader } from "./wsrvLoader";
+import LogoImage from "./logo-image";
 
 const LogoLink = () => {
   return (
     <Link href="/" className="me-8 transition-opacity mt-[3px] items-center ease-in duration-200 flex gap-1.5 hover:opacity-80 ">
-      <Image priority fill={false} loader={createWsrvLoader({})} src="https://static.devilsect.com/devilsect-logo.png"
-        width={20}
-        height={0}
-        alt="Website logo"
+      <LogoImage
+        width={25}
+        height={25}
         className="ml-2 w-auto h-7 xs:h-8"
       />
       <p className="text-base xs:text-2xl uppercase tracking-widest bg-gradient-to-r from-red-700/80 to-primary/70 bg-clip-text text-transparent font-tilt-warp">

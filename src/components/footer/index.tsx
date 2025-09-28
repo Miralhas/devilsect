224c6fc6701dec5a114613ebcc699cf43f2a3602
Miralhas <victor2.miralhas@gmail.com>
2025-09-28 00:07:01 -0300

    feat: not found page

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import LogoImage from "../logo-image";
 
+const CURRENT_YEAR = new Date().getUTCFullYear()
+
 const Footer = () => {
   return (
     <div className="w-full bg-zinc-950 border-b-2 border-secondary shadow-2xl">
@@ -17,7 +19,7 @@ const Footer = () => {
             DevilSect
           </span>
         </Link>
-        <p className="text-xs md:text-sm text-foreground/80 font-medium leading-none ">Copyright 2025 © Devil Sect</p>
+        <p className="text-xs md:text-sm text-foreground/80 font-medium leading-none ">Copyright {CURRENT_YEAR} © Devil Sect</p>
         <div className="space-x-3 text-xs md:text-sm">
           <Link href="/privacy" className="text-foreground/80 leading-none font-medium transition-colors duration-200 ease-out hover:text-foreground">Privacy Policy</Link>
           <Link href="/contact" className="text-foreground/80 leading-none font-medium transition-colors duration-200 ease-out hover:text-foreground">Contact</Link>

9a6a5aebe4442c29c5235464edfe5d9f6686bdf3
Miralhas <victor2.miralhas@gmail.com>
2025-10-02 12:11:23 -0300

    feat: add blur data on home page novels

--- src/components/home/ranking-section/desktop-ranking.tsx
+++ src/components/home/ranking-section/desktop-ranking.tsx
@@ -14,6 +14,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { getBlurData } from "@/lib/get-blur-data";
 
 type DesktopRankingProps<T> = {
   queries: PaginatedQuery<T>[]
@@ -59,45 +60,49 @@ const DesktopRanking = ({ queries }: DesktopRankingProps<NovelSummary[]>) => {
             </div>
             {queries.map((query, i) => (
               <div className="col-span-1 h-full flex flex-col gap-3" key={i}>
-                {query.results.map((novel, i) => (
-                  <Link href={`/novels/${novel.slug}`} key={novel.id} className="flex gap-2 transition-all duration-200 shadow-sm shadow-accent/30 group hover:shadow-md hover:shadow-accent/80 hover:text-accent ease-in-out hover:translate-x-1 hover:-translate-y-1" >
-                    <div className="aspect-[3/4] h-[75px] overflow-hidden rounded-sm relative group">
-                      <DynamicBlurImage
-                        src={`${env.NEXT_PUBLIC_BASE_URL}/novels/${novel.slug}/image`}
-                        alt={novel.title + " cover"}
-                        className="w-full h-auto object-cover object-center align-middle duration-300 transition-transform ease-in-out group-hover:scale-110"
-                        sizes="(max-width: 768px) 25vw, 10vw"
-                        default={`https://static.devilsect.com/No-Image-Placeholder.svg`}
-                      />
-                    </div>
-                    <div className="flex w-max text-sm font-medium tracking-tighter">
-                      <p className={rankingClassnames(i)}>{leadingZero(i + 1, 2)}</p>
-                    </div>
-                    <div className="flex-1 gap-1.5 self-stretch flex flex-col">
-                      <Tooltip delayDuration={300}>
-                        <TooltipTrigger className="text-start cursor-pointer">
-                          <p className="font-bold line-clamp-1 text-sm text-[12px] md:text-[14px] overflow-hidden capitalize">
-                            {novel.title}
-                          </p>
-                        </TooltipTrigger>
-                        <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
-                          {novel.title}
-                        </TooltipContent>
-                      </Tooltip>
-                      <div className="flex gap-3 items-center text-muted-foreground">
-                        <div className="text-xs font-bold flex gap-1 items-center">
-                          <StarIcon className="size-3 text-yellow-500" fill="#D3AF37" />
-                          <span className="text-xs text-[11px]">{novel.ratingValue ?? 'N/A'}</span>
-                        </div>
-                        <div className="flex items-center gap-1  font-semibold">
-                          <BookOpenText className="size-4" />
-                          <p className="text-sm text-[12px] overflow-hidden line-clamp-1">{novel.chaptersCount} Chapters</p>
-                        </div>
+                {query.results.map(async (novel, i) => {
+                  const { base64 } = await getBlurData(`${env.NEXT_PUBLIC_BASE_URL}/novels/${novel.slug}/image`);
+                  return (
+                    <Link href={`/novels/${novel.slug}`} key={novel.id} className="flex gap-2 transition-all duration-200 shadow-sm shadow-accent/30 group hover:shadow-md hover:shadow-accent/80 hover:text-accent ease-in-out hover:translate-x-1 hover:-translate-y-1" >
+                      <div className="aspect-[3/4] h-[75px] overflow-hidden rounded-sm relative group">
+                        <DynamicBlurImage
+                          src={`${env.NEXT_PUBLIC_BASE_URL}/novels/${novel.slug}/image`}
+                          alt={novel.title + " cover"}
+                          className="w-full h-auto object-cover object-center align-middle duration-300 transition-transform ease-in-out group-hover:scale-110"
+                          sizes="(max-width: 768px) 25vw, 10vw"
+                          default={`https://static.devilsect.com/No-Image-Placeholder.svg`}
+                          blurData={base64}
+                        />
                       </div>
-                      <p className="text-sm text-[12px] font-semibold text-muted-foreground">{statusMap[novel.status]}</p>
-                    </div>
-                  </Link>
-                ))}
+                      <div className="flex w-max text-sm font-medium tracking-tighter">
+                        <p className={rankingClassnames(i)}>{leadingZero(i + 1, 2)}</p>
+                      </div>
+                      <div className="flex-1 gap-1.5 self-stretch flex flex-col">
+                        <Tooltip delayDuration={300}>
+                          <TooltipTrigger className="text-start cursor-pointer">
+                            <p className="font-bold line-clamp-1 text-sm text-[12px] md:text-[14px] overflow-hidden capitalize">
+                              {novel.title}
+                            </p>
+                          </TooltipTrigger>
+                          <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
+                            {novel.title}
+                          </TooltipContent>
+                        </Tooltip>
+                        <div className="flex gap-3 items-center text-muted-foreground">
+                          <div className="text-xs font-bold flex gap-1 items-center">
+                            <StarIcon className="size-3 text-yellow-500" fill="#D3AF37" />
+                            <span className="text-xs text-[11px]">{novel.ratingValue ?? 'N/A'}</span>
+                          </div>
+                          <div className="flex items-center gap-1  font-semibold">
+                            <BookOpenText className="size-4" />
+                            <p className="text-sm text-[12px] overflow-hidden line-clamp-1">{novel.chaptersCount} Chapters</p>
+                          </div>
+                        </div>
+                        <p className="text-sm text-[12px] font-semibold text-muted-foreground">{statusMap[novel.status]}</p>
+                      </div>
+                    </Link>
+                  )
+                })}
               </div>
             ))}
           </div>
--- src/components/home/recently-added-chapters/chapters-grid.tsx
+++ src/components/home/recently-added-chapters/chapters-grid.tsx
@@ -6,6 +6,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { env } from "@/env";
+import { getBlurData } from "@/lib/get-blur-data";
 import { PaginatedQuery } from "@/types/pagination";
 import { RecentlyAddedChapter } from "@/types/recently-added-chapters";
 import { formatDate } from "@/utils/date-utils";
@@ -17,64 +18,68 @@ const ChaptersGrid = ({ chapters }: { chapters: PaginatedQuery<RecentlyAddedChap
   return (
     <section className="space-y-4">
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-3 md:gap-y-2">
-        {chapters.results.map(chapter => (
-          <div className="group flex gap-2 relative items-center px-1 col-span-1 hover:bg-secondary/60 rounded-md" key={chapter.id}>
-            <div className="h-[65px] min-w-[50px] overflow-hidden rounded-r-sm relative">
-              <DynamicBlurImage
-                fill
-                sizes="(max-width: 768px) 33vw, 5vw"
-                alt={`${chapter.novelTitle} cover`}
-                className="object-cover w-full transition-transform group-hover:scale-105 duration-200 ease-in-out"
-                src={`${env.NEXT_PUBLIC_BASE_URL}/novels/${chapter.novelSlug}/image`}
-                default={`https://static.devilsect.com/No-Image-Placeholder.svg`}
-              />
-              <BookCoverOverlay />
-            </div>
-            <div className="space-y-0.5">
-              <Tooltip delayDuration={300}>
-                <TooltipTrigger className="flex cursor-pointer text-start">
-                  <Link
-                    href={`/novels/${chapter.novelSlug}`}
-                    className="transition-colors line-clamp-1 capitalize text-zinc-300/70 text-sm text-[13px] leading-4"
-                  >
+        {chapters.results.map(async (chapter) => {
+          const { base64 } = await getBlurData(`${env.NEXT_PUBLIC_BASE_URL}/novels/${chapter.novelSlug}/image`);
+          return (
+            <div className="group flex gap-2 relative items-center px-1 col-span-1 hover:bg-secondary/60 rounded-md" key={chapter.id}>
+              <div className="h-[65px] min-w-[50px] overflow-hidden rounded-r-sm relative">
+                <DynamicBlurImage
+                  fill
+                  sizes="(max-width: 768px) 33vw, 5vw"
+                  alt={`${chapter.novelTitle} cover`}
+                  className="object-cover w-full transition-transform group-hover:scale-105 duration-200 ease-in-out"
+                  src={`${env.NEXT_PUBLIC_BASE_URL}/novels/${chapter.novelSlug}/image`}
+                  default={`https://static.devilsect.com/No-Image-Placeholder.svg`}
+                  blurData={base64}
+                />
+                <BookCoverOverlay />
+              </div>
+              <div className="space-y-0.5">
+                <Tooltip delayDuration={300}>
+                  <TooltipTrigger className="flex cursor-pointer text-start">
+                    <Link
+                      href={`/novels/${chapter.novelSlug}`}
+                      className="transition-colors line-clamp-1 capitalize text-zinc-300/70 text-sm text-[13px] leading-4"
+                    >
+                      {chapter.novelTitle}
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
                     {chapter.novelTitle}
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
-                  {chapter.novelTitle}
-                </TooltipContent>
-              </Tooltip>
+                  </TooltipContent>
+                </Tooltip>
 
-              <Tooltip delayDuration={300}>
-                <TooltipTrigger className="flex cursor-pointer text-start">
-                  <Link
-                    href={`/novels/${chapter.novelSlug}/${chapter.slug}`}
-                    className="text-sm text-[13px] line-clamp-1 w-full transition-colors text-zinc-200 font-semibold"
-                  >
+                <Tooltip delayDuration={300}>
+                  <TooltipTrigger className="flex cursor-pointer text-start">
+                    <Link
+                      href={`/novels/${chapter.novelSlug}/${chapter.slug}`}
+                      className="text-sm text-[13px] line-clamp-1 w-full transition-colors text-zinc-200 font-semibold"
+                    >
+                      {chapter.title}
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
                     {chapter.title}
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent className="capitalize text-center bg-secondary border border-zinc-50/10 text-zinc-200">
-                  {chapter.title}
-                </TooltipContent>
-              </Tooltip>
-              <div className="flex flex-col text-muted-foreground text-xs">
-                <span className="text-muted-foreground text-xs">{chapter.author}</span>
-                <div className="flex items-center gap-1.5">
-                  <p className="font-light inline-flex gap-1 items-center">
-                    <BookOpenText className="size-3" />
-                    <span>Ch. {chapter.chapterNumber}</span>
-                  </p>
-                  -
-                  <p className="inline-flex gap-1 items-center">
-                    <TimerIcon className="size-3" />
-                    <span>{formatDate(chapter.createdAt)}</span>
-                  </p>
+                  </TooltipContent>
+                </Tooltip>
+                <div className="flex flex-col text-muted-foreground text-xs">
+                  <span className="text-muted-foreground text-xs">{chapter.author}</span>
+                  <div className="flex items-center gap-1.5">
+                    <p className="font-light inline-flex gap-1 items-center">
+                      <BookOpenText className="size-3" />
+                      <span>Ch. {chapter.chapterNumber}</span>
+                    </p>
+                    -
+                    <p className="inline-flex gap-1 items-center">
+                      <TimerIcon className="size-3" />
+                      <span>{formatDate(chapter.createdAt)}</span>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </section>
   )

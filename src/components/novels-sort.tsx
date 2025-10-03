1aae3fc4389fe4314d3abea7024fe5d40da1f161
Miralhas <victor2.miralhas@gmail.com>
2025-10-03 18:59:19 -0300

    feat: author page

---
+++ src/components/novels-sort.tsx
@@ -0,0 +1,30 @@
+'use client'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { NovelSearchParams, nuqsNovelSummariesParams, sortKeyParams } from "@/lib/schemas/search-params/novel-summaries-params-schema";
+import { cn } from "@/utils/common-utils";
+import { capitalize } from "@/utils/string-utils";
+import { useQueryStates } from "nuqs";
+
+const NovelsSort = ({ className }: { className?: string }) => {
+  const [params, setParams] = useQueryStates(nuqsNovelSummariesParams);
+  return (
+    <Select value={params.sort} onValueChange={(val: NovelSearchParams["sort"]) => setParams({ ...params, sort: val })}>
+      <SelectTrigger className={cn(className)}>
+        <SelectValue placeholder="Sort by" />
+      </SelectTrigger>
+      <SelectContent>
+        {sortKeyParams.map((s, index) => (
+          <SelectItem key={index} value={s} className="capitalize">{capitalize(s)}</SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default NovelsSort;

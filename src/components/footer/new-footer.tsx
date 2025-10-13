110d35969b4c6587ad1c08e2102038f7782f961a
Miralhas <victor2.miralhas@gmail.com>
2025-10-13 20:11:28 -0300

    style

--- src/components/footer/new-footer.tsx
+++ src/components/footer/new-footer.tsx
@@ -28,30 +28,28 @@ const links = {
 
 const NewFooter = () => {
   return (
-    <div className="w-full bg-zinc-950 border-y-2 border-zinc-50/15 shadow-2xl pt-8 md:pt-16">
+    <div className="w-full bg-zinc-950 border-y-2 border-zinc-50/15 shadow-2xl">
       <div className="max-w-[1280px] mx-auto px-4">
-        <footer className="space-y-2">
+        <footer className="space-y-2 py-7 md:py-14">
           <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch md:justify-between w-full gap-5 md:gap-0">
-            <div className="flex items-center">
-              <Link href="/" className="transition-opacity items-center ease-in duration-200 flex gap-1.5 hover:opacity-80">
-                <LogoImage
-                  width={40}
-                  height={40}
-                  className="w-auto h-16"
-                />
-                <div>
-                  <span className="text-base md:text-3xl uppercase tracking-widest font-tilt-warp text-accent-foreground/85">
-                    DevilSect
-                  </span>
-                  <p className="text-xs md:text-[13px] text-zinc-300/90 leading-none">Free webnovel digital library</p>
-                </div>
-              </Link>
-            </div>
+            <Link href="/" className="transition-opacity items-center ease-in duration-200 flex gap-1.5 hover:opacity-80">
+              <LogoImage
+                width={40}
+                height={40}
+                className="w-auto h-18"
+              />
+              <div>
+                <span className="text-base md:text-3xl uppercase tracking-widest font-tilt-warp text-accent-foreground/85">
+                  DevilSect
+                </span>
+                <p className="text-xs md:text-[13px] text-zinc-300/90 leading-none">Free webnovel digital library</p>
+              </div>
+            </Link>
             <div className="flex justify-between md:justify-start md:gap-24 w-full md:w-auto">
               {Object.entries(links).map(([k, v], i) => (
                 <div className="space-y-1 text-muted-foreground text-sm" key={i}>
-                  <p className="text-foreground/90 capitalize ml-3">{k}</p>
-                  <div className="ml-3 flex flex-col gap-0.5 font-light text-xs md:text-sm">
+                  <p className="text-foreground/90 capitalize ml-3 md:ml-0">{k}</p>
+                  <div className="ml-3 md:ml-0 flex flex-col gap-0.5 font-light text-xs md:text-sm">
                     {v.map((link, idx) => (
                       <Link className="group inline-flex text-muted-foreground transition-all hover:text-zinc-200 duration-200 cursor-pointer ease-in hover:translate-x-0.25 hover:-translate-y-0.25 relative underline decoration-[0.25px] underline-offset-3 gap-0.25" key={idx} href={link.href}>{link.name}
                         <ArrowRight className="size-3 rotate-310 opacity-70" />
@@ -65,12 +63,16 @@ const NewFooter = () => {
           </div>
         </footer>
       </div>
-      <Separator orientation="horizontal" className="mt-3" />
+      <Separator orientation="horizontal" />
       <div className="mx-auto max-w-[1280px] p-4">
-        <div className="flex justify-between text-sm text-[13px] items-center text-muted-foreground">
+        <div className="flex text-sm text-[13px] items-center text-muted-foreground gap-4">
           <p className="text-xs md:text-sm text-zinc-500 italic font-medium leading-none mt-auto hidden md:block">Copyright {CURRENT_YEAR} © Devil Sect</p>
-          <Link href="/contact-us" className="text-sm text-[13px] text-muted-foreground block md:hidden transition-all hover:text-zinc-200 duration-200 cursor-pointer ease-in hover:translate-x-0.25 hover:-translate-y-0.25 relative">Contact Us</Link>
-          <Link href="/sitemap.xml" className="text-sm text-[13px] text-muted-foreground transition-all hover:text-zinc-200 duration-200 cursor-pointer ease-in hover:translate-x-0.25 hover:-translate-y-0.25 relative">Sitemap</Link>
+          <Link href="/contact-us" className="text-sm text-[13px] text-muted-foreground transition-all hover:text-zinc-200 duration-200 cursor-pointer ease-in hover:translate-x-0.25 hover:-translate-y-0.25 relative mr-auto md:mr-0 md:ml-auto">
+            Contact Us
+          </Link>
+          <Link href="/sitemap.xml" className="text-sm text-[13px] text-muted-foreground transition-all hover:text-zinc-200 duration-200 cursor-pointer ease-in hover:translate-x-0.25 hover:-translate-y-0.25 relative">
+            Sitemap
+          </Link>
         </div>
       </div>
     </div>

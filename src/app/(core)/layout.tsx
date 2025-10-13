26f1dd0961bed00b2dfc89e8fb1f9e13dcf0147c
Miralhas <victor2.miralhas@gmail.com>
2025-10-13 13:19:35 -0300

    feat: new footer

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/footer";
+import NewFooter from "@/components/footer/new-footer";
 import GlobalLoginDialog from "@/components/global-login-dialog";
 import Header from "@/components/navbar/header";
 import { ReaderSettingsProvider } from "@/contexts/reader-settings-context";
@@ -9,7 +9,7 @@ const CoreLayout = async ({ children }: PropsWithChildren) => {
     <ReaderSettingsProvider>
       <Header />
       {children}
-      <Footer />
+      <NewFooter />
       <GlobalLoginDialog />
     </ReaderSettingsProvider>
   )

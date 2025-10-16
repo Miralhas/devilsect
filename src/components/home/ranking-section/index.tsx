511ccb2698f9468a15df1aa210ed5170afeae395
Miralhas <victor2.miralhas@gmail.com>
2025-10-16 19:28:03 -0300

    fix: ranking incorrect queries

--- src/components/home/ranking-section/index.tsx
+++ src/components/home/ranking-section/index.tsx
@@ -3,18 +3,26 @@ import { SortKey } from "@/lib/schemas/search-params/novel-summaries-params-sche
 import { getNovelSummaries } from "@/service/novels/api/get-novel-summaries";
 import { Trophy } from "lucide-react";
 import DesktopRanking from "./desktop-ranking";
+import { arrayShuffler } from "@/utils/array-utils";
 
 const RankingSection = async () => {
-  const bayesian = getNovelSummaries({ size: 10, sort: SortKey.BAYESIAN_RANKING });
-  const views = getNovelSummaries({ size: 10, sort: SortKey.MOST_VIEWED });
-  const trend = getNovelSummaries({ size: 10, sort: SortKey.MOST_RATED });
+  const bayesianP = getNovelSummaries({ size: 10, sort: SortKey.BAYESIAN_RANKING });
+  const viewsP = getNovelSummaries({ size: 10, sort: SortKey.MOST_VIEWED });
+  const newTrendsP = getNovelSummaries({ size: 10, sort: SortKey.NEWEST_RELEASES });
+  const ratedP = getNovelSummaries({ size: 10, sort: SortKey.MOST_RATED });
 
-  const queries = await Promise.all([bayesian, trend, views, views]);
+  const [bayesian, newTrends, views, rated] = await Promise.all([bayesianP, newTrendsP, viewsP, ratedP]);
+
+  arrayShuffler(views.results);
+
+  newTrends.results = newTrends.results.sort((a, b) => {
+    return b.bayesianScore - a.bayesianScore
+  });
 
   return (
     <section className="space-y-6">
       <SectionHeader icon={Trophy} title="Ranking" viewMore={{ href: "/ranking", title: "View More" }} />
-      <DesktopRanking queries={queries} />
+      <DesktopRanking queries={[bayesian, newTrends, views, rated]} />
     </section>
   )
 }
--- src/utils/array-utils.ts
+++ src/utils/array-utils.ts
@@ -3,3 +3,19 @@ export function arrayChunker<T>(arr: T[], size: number): T[][] {
   return [...Array(Math.ceil(arr.length / size))].map((_, i) => arr.slice(size * i, size + size * i)
   );
 };
+
+export const arrayShuffler = <T>(array: T[]) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}

67acdda261f289a2a930ff37eec2d048cee12df2
Miralhas <victor2.miralhas@gmail.com>
2025-11-08 13:41:45 -0300

    fix: remove wrong tailwind  class

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="h-screen bg-cover bg-bg-no-repeat overlay">
+    <div className="h-screen bg-[url(https://wsrv.nl/?url=https://static.devilsect.com/divine-tribulation.jpg&w=1920&q=25&output=webp&fit=cover&dpr=2&maxage=1y&il=true&af=true)] bg-no-repeat bg-cover overlay">
       <div className="h-full w-full grid place-items-center p-4">
         <div className="space-y-2 max-w-lg text-center">
           <h1 className="font-tilt-warp text-7xl font-bold">404</h1>
           <p className="font-medium">This page either does not exist or has mysteriously disappeared.</p>
           <p className="text-xs text-muted-foreground">Or perhaps it was destroyed after a divine tribulation.</p>
-          <Button asChild variant="cool" className="mt-4">
+          <Button asChild variant="cool" className="mt-4 h-10 w-full max-w-[180px]">
             <Link href="/">Home Page</Link>
           </Button>
         </div>

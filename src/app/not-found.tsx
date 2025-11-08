24cef3e69e76d93c8722b62283c9c916d3b6d8cf
Miralhas <victor2.miralhas@gmail.com>
2025-11-08 13:22:43 -0300

    fix: wrong image link in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="h-screen bg-[url(https://wsrv.nl/?url=https%3A%2F%2Fstatic.devilsect.com%2Fdivine-tribulation.jpg&w=3840&q=75&output=webp&fit=cover&dpr=2&maxage=15d&il=true&af=true)] bg-cover bg-bg-no-repeat overlay">
+    <div className="h-screen bg-[url(https://wsrv.nl/?url=https://static.devilsect.com/divine-tribulation.jpg&w=3840&q=75&output=webp&fit=cover&dpr=2&maxage=15d&il=true&af=true)] bg-cover bg-bg-no-repeat overlay">
       <div className="h-full w-full grid place-items-center p-4">
         <div className="space-y-2 max-w-lg text-center">
           <h1 className="font-tilt-warp text-7xl font-bold">404</h1>

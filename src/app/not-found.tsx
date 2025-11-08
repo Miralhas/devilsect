6f24003c888f034dee8f8ba0a86b20d033510ed6
Miralhas <victor2.miralhas@gmail.com>
2025-11-08 13:55:13 -0300

    feat: remove link component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import HomePageButton from "@/components/home-page-button";
 
 const BG_IMAGE = `https://wsrv.nl/?url=https://static.devilsect.com/divine-tribulation.jpg&w=1920&q=25&output=webp&fit=cover&dpr=2&maxage=1y&il=true&af=true`;
 
@@ -13,9 +12,7 @@ const NotFound = () => {
           <h1 className="font-tilt-warp text-7xl font-bold">404</h1>
           <p className="font-medium">This page either does not exist or has mysteriously disappeared.</p>
           <p className="text-xs text-muted-foreground">Or perhaps it was destroyed after a divine tribulation.</p>
-          <Button asChild variant="cool" className="mt-4 h-10 w-full max-w-[180px]">
-            <Link href="/">Home Page</Link>
-          </Button>
+          <HomePageButton className="mt-4 w-full max-w-[180px]" />
         </div>
       </div>
     </div>

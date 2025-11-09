c3510b974a2ceb96accc9c459d461fb964027027
Miralhas <victor2.miralhas@gmail.com>
2025-11-09 15:55:18 -0300

    feat: new banners

--- src/components/home/hero-section/carousel-novel/banners-data.ts
+++ src/components/home/hero-section/carousel-novel/banners-data.ts
@@ -2,6 +2,24 @@ import { NovelBanner } from "@/types/novel";
 
 export const BANNERS: NovelBanner[] = [
   {
+    name: "Lord of the Mysteries",
+    description: `With the rising tide of steam power and machinery, who can hope to become a Beyonder? Shrouded in the fog of history and darkness, what lurking evil murmurs into our ears? Waking up to face a string of mysteries, Zhou Mingrui finds himself reincarnated as Klein Moretti in an alternate Victorian-era world. He discovers a realm filled with machinery, cannons, dreadnoughts, airships and various contraptions, alongside Potions, Divination, Hexes, Tarot Cards and Sealed Artifacts…`,
+    src: "https://static.devilsect.com/lotm-banner.png",
+    status: "COMPLETED",
+    mc: "https://static.devilsect.com/klein.png",
+    slug: "barbarian-quest",
+    className: "-right-12 md:-right-2 max-w-[280px] top-auto -bottom-15 md:-bottom-25"
+  },
+  {
+    name: "Mistborn: The Final Empire",
+    description: `For a thousand years the ash fell and no flowers bloomed. For a thousand years the Skaa slaved in misery and lived in fear. For a thousand years the Lord Ruler, the “Sliver of Infinity,” reigned with absolute power and ultimate terror, divinely invincible. Then, when hope was so long lost that not even its memory remained, a terribly scarred, heart-broken half-Skaa rediscovered it in the depths of the Lord Ruler's most hellish prison.`,
+    src: "https://static.devilsect.com/mistborn-banner.webp",
+    status: "COMPLETED",
+    mc: "https://static.devilsect.com/vin-and-kelsier.png",
+    slug: "mistborn-the-final-empire",
+    className: "-right-10 md:-right-16 max-w-[400px] -top-5 md:-top-14"
+  },
+  {
     name: "Reverend Insanity",
     description: `Human beings are the very spirit of all life, while Gu embody the essence of heaven and earth. When one’s worldview, values, and philosophy become twisted, they are no longer human but demon reborn. The past fades into a fleeting dream, yet a name may echo anew in time. This is the tale of a time traveler, reborn endlessly, forging his path through a world shaped by the mystical power of Gu (creatures of legends and malice).`,
     src: "https://static.devilsect.com/reverend-insanity-banner.jpg",
@@ -24,9 +42,9 @@ export const BANNERS: NovelBanner[] = [
     description: `Darrow is a miner and a Red, a member of the lowest caste in the color-coded society of the future. Like his fellow Reds, he digs all day, believing that he and his people are making the surface of the planet livable for future generations. Darrow has never seen the sky. Yet he spends his life willingly, knowing that his blood and sweat will one day result in a better future for his children.`,
     src: "https://static.devilsect.com/red-rising-banner-opacity.jpg",
     status: "COMPLETED",
-    mc: "https://static.devilsect.com/darrow-of-lykos.png",
+    mc: "https://static.devilsect.com/darrow-red.png",
     slug: "red-rising",
-    className: "md:right-5 max-w-[240px]"
+    className: "-right-10 md:-right-24 max-w-[500px] -top-5 md:-top-10"
   },
   {
     name: "Barbarian Quest",
@@ -37,14 +55,4 @@ export const BANNERS: NovelBanner[] = [
     slug: "barbarian-quest",
     className: "-right-12 md:-right-15 max-w-[250px] md:max-w-[400px] md:top-auto md:-bottom-64"
   },
-  {
-    name: "Lord of the Mysteries",
-    description: `With the rising tide of steam power and machinery, who can hope to become a Beyonder? Shrouded in the fog of history and darkness, what lurking evil murmurs into our ears? Waking up to face a string of mysteries, Zhou Mingrui finds himself reincarnated as Klein Moretti in an alternate Victorian-era world. He discovers a realm filled with machinery, cannons, dreadnoughts, airships and various contraptions, alongside Potions, Divination, Hexes, Tarot Cards and Sealed Artifacts…`,
-    src: "https://static.devilsect.com/lotm-banner.png",
-    status: "COMPLETED",
-    mc: "https://static.devilsect.com/klein.png",
-    slug: "barbarian-quest",
-    className: "-right-12 md:-right-2 max-w-[280px] top-auto -bottom-15 md:-bottom-25"
-  },
-
 ] 
